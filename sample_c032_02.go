7a40a9abb089e9dab8927666e9133c777fe517ca
tjtjtjtj <nemunemutjtjtjtj@gmail.com>
2017-08-20 15:23:41 +0100

    update:c032_032

--- sample_c032_02.go
+++ sample_c032_02.go
@@ -36,25 +36,18 @@ func (c *Customer) Points_cal(items []Item) {
 	}
 }
 
-func Init_item(items []Item) {
-	//func Init_item() []Item {
-	fmt.Printf("%p\n", items)
-	items = []Item{
+func Init_item() []Item {
+	items := []Item{
 		{no: "0", name: "foods", point_add: 5},
 		{no: "1", name: "books", point_add: 3},
 		{no: "2", name: "clothes", point_add: 2},
 		{no: "3", name: "others", point_add: 1},
 	}
-	fmt.Printf("%p\n", items)
-	//return items
+	return items
 }
 
 func main() {
-	var items []Item
-	Init_item(items)
-	//items = Init_item()
-	//fmt.Print(len(items))
-	//fmt.Print(items[0].name)
+	items := Init_item()
 
 	var person Customer
 	person.prices = make([]int, len(items))
